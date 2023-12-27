ec1fc8ee8897afc8e6610adcfa12515d89ccf547
Cecilia <ccchimento@gmail.com>
2023-12-27 12:49:37 -0700

    book reviews started

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-fetch('http://localhost:3000/books')
+fetch('http://localhost:3000/books/')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+
+fetch('http://localhost:3000/reviews/')
     .then(resp => resp.json())
     .then(data => console.log(data))
