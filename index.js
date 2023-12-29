f2d5c15a62a92febe351653fb507b2bb30e0133f
Michaelp720 <151689186+Michaelp720@users.noreply.github.com>
2023-12-29 15:36:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 fetch('http://localhost:3000/books')
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => renderBooks(data))
+
+//DOM locations 
+const bookListLoc = document.querySelector('#book-list');
+
+const coverLoc = document.querySelector('#detail-image');
+const titleLoc = document.querySelector('#title');
+const yearReleasedLoc = document.querySelector('#year-released');
+const pageNumberLoc = document.querySelector('#page-numbers');
+const summaryLoc = document.querySelector('#summary');
+const ratingLoc = document.querySelector('#rating');
+const userRatingLoc = document.querySelector('#user-rating');
+const reviewsLoc = document.querySelector('#review-display');
+
+function renderBooks(bookDataObj){
+    //add user rating to each object in array 'books'
+    const booksArr = bookDataObj.books;
+
+    
+
+    booksArr.forEach(book => {
+
+        //adds book cover to book list
+        const bookImg = document.createElement('img');
+        bookImg.src = book.cover;
+        bookListLoc.appendChild(bookImg);
+
+        //adds click event on each element in book list to display clicked book
+        bookImg.addEventListener('click', displayBook);
+
+        function displayBook(){
+            coverLoc.src = book.cover;
+            titleLoc.innerText = book.title;
+            yearReleasedLoc.innerText = book.released;
+            pageNumberLoc.innerText = book.pages;
+            summaryLoc.innerText = book.summary;
+            ratingLoc.innerText = book.rating;
+        }
+        
+    });
+
+}
